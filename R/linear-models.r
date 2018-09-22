6ee9ef3a61b0687dee375e7b98905f4b3c14810a
Tianyuan Zhang <tianyuanzhang@Tianyuans-MacBook-Pro.local>
2018-09-22 01:23:14 -0400

    Changed back to the first version.

--- R/linear-models.r
+++ R/linear-models.r
@@ -20,29 +20,30 @@ linear_model <- function(formula, data) {
   Y<-data[,var[1]]
   
   #use qr function to decompose the matrix and find coefficients as beta
-  beta<-qr.coef(qr(X),Y)
+  beta<-qr.solve(X,Y)
+  beta[which(beta==0)] <- NA
   
   #fit the coponents of lm model into a list, and set the class as lm
   #if I cannot get the value, I would just assign it as NULL ---- got help from Hongyu Li
   
-  lm_result<- list()
-  
-  lm_result$coefficients = beta
-  lm_result$dr.residual = nrow(X) - ncol(X)
-  lm_result$call = call("linear_model", formula)
-  lm_result$terms = terms(x = formula, data = data)
-  lm_result$qr = qr(X)
-  lm_result$fitted.values =NULL
-  lm_result$residuals = NULL
-  lm_result$y = Y
-  lm_result$x = X
-  lm_result$rank = NULL
-  lm_result$model = NULL
-  lm_result$weights = NULL
-  lm_result$contrasts = NULL
-  lm_result$xlevels = NULL
-  lm_result$offset = NULL
-  lm_result$na.action = NULL
+  lm_result<- list(
+    coefficients = beta,
+    dr.residual = nrow(X) - ncol(X),
+    call = NULL,
+    terms = terms(x = formula, data = data),
+    qr = qr(X),
+    fitted.values =NULL,
+    residuals = NULL,
+    y = Y,
+    x = X,
+    rank = NULL,
+    model = NULL,
+    weights = NULL,
+    contrasts = NULL,
+    xlevels = NULL,
+    offset = NULL,
+    na.action = NULL
+  )
   
   class(lm_result) = 'lm'
   
@@ -51,6 +52,6 @@ linear_model <- function(formula, data) {
   
 }
 
-load("/Users/tianyuanzhang/Desktop/2018_bis557/bis557/data/lm_patho.rda")
-linear_model(y~., data = lm_patho)
+#load("/Users/tianyuanzhang/Desktop/2018_bis557/bis557/data/lm_patho.rda")
+#linear_model(y~., data = lm_patho)
 
