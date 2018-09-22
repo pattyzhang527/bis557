09ed5547e5580ec3525dec4fab975b8f275b55f5
Tianyuan Zhang <tianyuanzhang@Tianyuans-MacBook-Pro.local>
2018-09-22 01:13:51 -0400

    fixed bug for version 3.

--- R/linear-models.r
+++ R/linear-models.r
@@ -25,24 +25,24 @@ linear_model <- function(formula, data) {
   #fit the coponents of lm model into a list, and set the class as lm
   #if I cannot get the value, I would just assign it as NULL ---- got help from Hongyu Li
   
-  lm_result<- list(
-    coefficients = beta,
-    dr.residual = nrow(X) - ncol(X),
-    call = call("linear_model", formula),
-    terms = terms(x = formula, data = data),
-    qr = qr(X),
-    fitted.values =NULL,
-    residuals = NULL,
-    y = Y,
-    x = X,
-    rank = NULL,
-    model = NULL,
-    weights = NULL,
-    contrasts = NULL,
-    xlevels = NULL,
-    offset = NULL,
-    na.action = NULL
-  )
+  lm_result<- list()
+  
+  lm_result$coefficients = beta
+  lm_result$dr.residual = nrow(X) - ncol(X)
+  lm_result$call = call("linear_model", formula)
+  lm_result$terms = terms(x = formula, data = data)
+  lm_result$qr = qr(X)
+  lm_result$fitted.values =NULL
+  lm_result$residuals = NULL
+  lm_result$y = Y
+  lm_result$x = X
+  lm_result$rank = NULL
+  lm_result$model = NULL
+  lm_result$weights = NULL
+  lm_result$contrasts = NULL
+  lm_result$xlevels = NULL
+  lm_result$offset = NULL
+  lm_result$na.action = NULL
   
   class(lm_result) = 'lm'
   
@@ -50,4 +50,7 @@ linear_model <- function(formula, data) {
   return(lm_result)
   
 }
+
+load("/Users/tianyuanzhang/Desktop/2018_bis557/bis557/data/lm_patho.rda")
+linear_model(y~., data = lm_patho)
 
