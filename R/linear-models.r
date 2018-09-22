ccb27ee78e32d00c95bd3fc342a04f7f2a56cf8f
Tianyuan Zhang <tianyuanzhang@Tianyuans-MacBook-Pro.local>
2018-09-22 11:16:36 -0400

    fixed check() bugs

--- R/linear-models.r
+++ R/linear-models.r
@@ -1,57 +1,32 @@
-
 #' Fit a linear model
 #'
 #' @description This function passes parameters to the lm function.
 #' @param formula a formula
 #' @param data a data.frame
 #' @return An lm object
-#' @importFrom stats lm
+#' @importFrom stats model.matrix terms
 #' @examples
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
 #' @export
-linear_model <- function(formula, data) {
-  #get the design matrix X
-  X<-model.matrix(formula, data)
-  
-  #extract the response variable Y from the above matrix
-  #since for every matrix, Y variable is the first cololum, we get
-  var<-all.vars(formula)
-  Y<-data[,var[1]]
-  
-  #use qr function to decompose the matrix and find coefficients as beta
-  beta<-qr.solve(X,Y)
-  beta[which(beta==0)] <- NA
-  
-  #fit the coponents of lm model into a list, and set the class as lm
-  #if I cannot get the value, I would just assign it as NULL ---- got help from Hongyu Li
-  
-  lm_result<- list(
-    coefficients = beta,
-    dr.residual = nrow(X) - ncol(X),
-    call = NULL,
-    terms = terms(x = formula, data = data),
-    qr = qr(X),
-    fitted.values =NULL,
-    residuals = NULL,
-    y = Y,
-    x = X,
-    rank = NULL,
-    model = NULL,
-    weights = NULL,
-    contrasts = NULL,
-    xlevels = NULL,
-    offset = NULL,
-    na.action = NULL
-  )
-  
-  class(lm_result) = 'lm'
-  
-  #get the final result
-  return(lm_result)
-  
-}
 
-#load("/Users/tianyuanzhang/Desktop/2018_bis557/bis557/data/lm_patho.rda")
-#linear_model(y~., data = lm_patho)
+linear_model <- function(formula, data) {
+  var = all.vars(formula)
+  y = data[, var[1]]
+  X = model.matrix(formula, data)
+  
+  X_QR = qr(X)
+  beta = solve.qr(X_QR, y)
+  beta[which(beta == 0)] = NA
+  
+  fitted = X %*% beta
+  residuals = y - fitted
+  linear_model_fit = list(coefficients = beta, residals = residuals, fitted.values = fitted,
+                          rank = ncol(X), weights = NULL, df.residual = nrow(X) - ncol(X), 
+                          call = call('lm', formula), terms = terms(x = formula, data = data),
+                          contrasts = NA, xlevels = NA, offset = NA, y = y, x = X, 
+                          model = formula, na.action = NA, qr = X_QR)
+  class(linear_model_fit) = 'lm'
+  return(linear_model_fit)
+}
 
