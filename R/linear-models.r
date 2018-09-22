ff9f3f0948acbdd917347965c848177ac7220739
Tianyuan Zhang <tianyuanzhang@Tianyuans-MacBook-Pro.local>
2018-09-22 12:02:30 -0400

    Homework 1

--- R/linear-models.r
+++ R/linear-models.r
@@ -11,22 +11,29 @@
 #' @export
 
 linear_model <- function(formula, data) {
-  var = all.vars(formula)
-  y = data[, var[1]]
-  X = model.matrix(formula, data)
+  #get the design matrix X
+  X<-model.matrix(formula, data)
   
-  X_QR = qr(X)
-  beta = solve.qr(X_QR, y)
-  beta[which(beta == 0)] = NA
+  #extract the response variable Y from the above matrix
+  #since for every matrix, Y variable is the first cololum, we get
+  var<-all.vars(formula)
+  Y<-data[,var[1]]
   
-  fitted = X %*% beta
-  residuals = y - fitted
-  linear_model_fit = list(coefficients = beta, residals = residuals, fitted.values = fitted,
-                          rank = ncol(X), weights = NULL, df.residual = nrow(X) - ncol(X), 
-                          call = call('lm', formula), terms = terms(x = formula, data = data),
-                          contrasts = NA, xlevels = NA, offset = NA, y = y, x = X, 
-                          model = formula, na.action = NA, qr = X_QR)
-  class(linear_model_fit) = 'lm'
-  return(linear_model_fit)
+  #use qr function to decompose the matrix and find coefficients as beta
+  beta<-qr.solve(X,Y)
+  beta[which(beta==0)] <- NA
+  
+  #fit the coponents of lm model into a list, and set the class as lm
+  #if I cannot get the value, I would just assign it as NULL (got help from Hongyu Li)
+  
+  lm_result = list(
+    coefficients = beta, residals = X %*% beta, fitted.values = Y - X %*% beta, rank = ncol(X), 
+    df.residual = nrow(X) - ncol(X), call = call('lm', formula),  weights = NULL,
+    y = Y, x = X, model = formula, na.action = NA, qr = qr(X),
+    terms = terms(x = formula, data = data), contrasts = NA, xlevels = NA, offset = NA
+    )
+  
+  class(lm_result) <- 'lm'
+  #get the final result
+  return(lm_result)
 }
-
